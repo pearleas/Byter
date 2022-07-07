5e41981fe07d41c8e7fea061898f8763a8057032
pearleas <bloothering@rambler.ru>
2022-07-07 16:36:47 +0300

    Done

---
+++ README.md
@@ -0,0 +1,7 @@
+# Byter
+
+Simple CLI tool to convert binary files into C-like BYTE Array.
+
+### Usage
+
+    byter.exe [file] [array name]
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
+func main() {
+	if len(os.Args) != 3 {
+		panic(fmt.Sprintf("Wrong usage, expected 3 arguments, got %d", len(os.Args)))
+	}
+
+	file, err := os.Open(os.Args[1])
+	defer file.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	output, err := os.Create(os.Args[2] + ".hpp")
+	defer output.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	readBytes, err := ioutil.ReadFile(os.Args[2])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	output.WriteString("#include <windows.h>\n\n inline BYTE " + os.Args[2] + "[] = {\n\t")
+	for _, singleByte := range readBytes {
+		output.WriteString(fmt.Sprintf("0x%02X, ", singleByte))
+	}
+	output.WriteString("\n}\n")
 }
